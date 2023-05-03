977c3208c1cafd639e1cdbe523428bdcd12a953a
tiago-trigo <tiagotrigo02@gmail.com>
2023-05-03 10:20:27 +0100

    Fix data order on Top10s

--- pages/🥇Top 10s.py
+++ pages/🥇Top 10s.py
@@ -18,6 +18,7 @@ with st.container():
 	st.write("Top ten track sections for various metrics")
 
 st.write("---")
+
 col1,col2 = st.columns(2)
 year = col1.selectbox("Select a year", years)
 month = col2.selectbox("Select a month", months)
@@ -28,67 +29,60 @@ select_options = ["Vehicle occupancy (monthly average)", "Vehicle occupancy (mor
 col1, col2 = st.columns(2)
 metric_type = col1.selectbox("Select a metric", ["Vehicle occupancy", "Passager load"])
 time_period = col2.selectbox("Select a time period", ["Monthly average", "Morning peak hour", "Evening peak hour"])
+
 st.write("---")
+
 metric = metric_type + time_period
 
 datasets = importing_data(year,month)
 
+col1, col2 = st.columns(2)
 
 if metric == "Vehicle occupancyMonthly average":
 	most_occ, least_occ = average_occupancy(datasets[-1])
-	with col1:
-		st.markdown("**Top 10 track sections with higher vehicle occupancy (monthly average)**")
-		st.table(most_occ)
-	with col2:
-		st.markdown("**Top 10 track sections with lower vehicle occupancy (monthly average)**")
-		st.table(least_occ)
+	col1.markdown("**Top 10 track sections with higher vehicle occupancy (monthly average)**")
+	col1.table(most_occ)
+	col2.markdown("**Top 10 track sections with lower vehicle occupancy (monthly average)**")
+	col2.table(least_occ)
 
 elif metric == "Vehicle occupancyMorning peak hour":
 	most_occ, least_occ = average_occupancy(datasets[2])
-	with col1:
-		st.markdown("**Top 10 track sections with higher vehicle occupancy (morning peak hour)**")
-		st.table(most_occ)
+	col1.markdown("**Top 10 track sections with higher vehicle occupancy (morning peak hour)**")
+	col1.table(most_occ)
 
-	with col2:
-		st.markdown("**Top 10 track sections with lower vehicle occupancy (morning peak hour)**")
-		st.table(least_occ)
+	col2.markdown("**Top 10 track sections with lower vehicle occupancy (morning peak hour)**")
+	col2.table(least_occ)
 
 elif metric == "Vehicle occupancyEvening peak hour":
 	most_occ, least_occ = average_occupancy(datasets[12])
-	with col1:
-		st.markdown("**Top 10 track sections with higher vehicle occupancy (evening peak hour)**")
-		st.table(most_occ)
 
-	with col2:
-		st.markdown("**Top 10 track sections with lower vehicle occupancy (evening peak hour)**")
-		st.table(least_occ)
+	col1.markdown("**Top 10 track sections with higher vehicle occupancy (evening peak hour)**")
+	col2.table(most_occ)
+
+	col2.markdown("**Top 10 track sections with lower vehicle occupancy (evening peak hour)**")
+	col2.table(least_occ)
 
 elif metric == "Passager loadMonthly average":
 	most_occ, least_occ = average_passager(datasets[-1])
-	with col1:
-		st.markdown("**Top 10 track sections with higher passager load (monthly average)**")
-		st.table(most_occ)
+	col1.markdown("**Top 10 track sections with higher passager load (monthly average)**")
+	col1.table(most_occ)
 
-	with col2:
-		st.markdown("**Top 10 track sections with lower passager load (monthly average)**")
-		st.table(least_occ)
+
+	col2.markdown("**Top 10 track sections with lower passager load (monthly average)**")
+	col2.table(least_occ)
 
 elif metric == "Passager loadMorning peak hour":
 	most_occ, least_occ = average_passager(datasets[2])
-	with col1:
-		st.markdown("**Top 10 track sections with higher passager load (morning peak hour)**")
-		st.table(most_occ)
+	col1.markdown("**Top 10 track sections with higher passager load (morning peak hour)**")
+	col1.table(most_occ)
 
-	with col2:
-		st.markdown("**Top 10 track sections with lower passager load (morning peak hour)**")
-		st.table(least_occ)
+	col2.markdown("**Top 10 track sections with lower passager load (morning peak hour)**")
+	col2.table(least_occ)
 
 elif metric == "Passager loadEvening peak hour":
 	most_occ, least_occ = average_passager(datasets[12])
-	with col1:
-		st.markdown("**Top 10 track sections with higher passager load (evening peak hour)**")
-		st.table(most_occ)
+	col1.markdown("**Top 10 track sections with higher passager load (evening peak hour)**")
+	col1.table(most_occ)
 
-	with col2:
-		st.markdown("**Top 10 track sections with lower passager load (evening peak hour)**")
-		st.table(least_occ)
+	col2.markdown("**Top 10 track sections with lower passager load (evening peak hour)**")
+	col2.table(least_occ)
